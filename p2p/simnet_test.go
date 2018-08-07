8761e43e9072c6eb1287ffabf7ff3c6e5ec09fe8
Ribao Wei <ribao@sliver.tv>
2018-08-07 16:11:29 -0700

    Fix race condition in simnet_test.

--- p2p/simnet_test.go
+++ p2p/simnet_test.go
@@ -34,13 +34,17 @@ func TestSimnetBroadcast(t *testing.T) {
 
 	e2.Broadcast("hello!")
 	time.Sleep(1 * time.Second)
+	msgHandler.lock.Lock()
 	sort.Strings(msgHandler.ReceivedMessages)
+	msgHandler.lock.Unlock()
 	assert.EqualValues([]string{"e1 <- hello!", "e2 <- hello!", "e3 <- hello!"}, msgHandler.ReceivedMessages)
 
 	msgHandler.ReceivedMessages = make([]string, 0)
 	e1.Broadcast("world!")
 	time.Sleep(1 * time.Second)
+	msgHandler.lock.Lock()
 	sort.Strings(msgHandler.ReceivedMessages)
+	msgHandler.lock.Unlock()
 	assert.EqualValues([]string{"e1 <- world!", "e2 <- world!", "e3 <- world!"}, msgHandler.ReceivedMessages)
 }
 
@@ -55,12 +59,16 @@ func TestSimnetSend(t *testing.T) {
 
 	e1.Send("e3", "hello!")
 	time.Sleep(1 * time.Second)
+	msgHandler.lock.Lock()
 	sort.Strings(msgHandler.ReceivedMessages)
+	msgHandler.lock.Unlock()
 	assert.EqualValues([]string{"e3 <- hello!"}, msgHandler.ReceivedMessages)
 
 	msgHandler.ReceivedMessages = make([]string, 0)
 	e1.Send("e1", "world!")
 	time.Sleep(1 * time.Second)
+	msgHandler.lock.Lock()
 	sort.Strings(msgHandler.ReceivedMessages)
+	msgHandler.lock.Unlock()
 	assert.EqualValues([]string{"e1 <- world!"}, msgHandler.ReceivedMessages)
 }
