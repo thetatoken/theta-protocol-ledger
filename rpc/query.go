1498df2ce1c2648d352c1d54f556661d9f81fe3b
Ribao Wei <ribao@sliver.tv>
2018-10-12 11:04:00 -0700

    rpc: add GetSplitContract()

--- rpc/query.go
+++ rpc/query.go
@@ -1,6 +1,7 @@
 package rpc
 
 import (
+	"encoding/hex"
 	"errors"
 	"net/http"
 
@@ -31,5 +32,31 @@ func (t *ThetaRPCServer) GetAccount(r *http.Request, args *GetAccountArgs, resul
 	}
 	result.Account = ledgerState.GetAccount(address)
 	result.Address = args.Address
+	return nil
+}
+
+// ------------------------------- GetSplitContract -----------------------------------
+
+type GetSplitContractArgs struct {
+	ResourceID string `json:"resource_id"`
+}
+
+type GetSplitContractResult struct {
+	*types.SplitContract
+}
+
+func (t *ThetaRPCServer) GetSplitContract(r *http.Request, args *GetSplitContractArgs, result *GetSplitContractResult) (err error) {
+	if args.ResourceID == "" {
+		return errors.New("ResourceID must be specified")
+	}
+	resourceID, err := hex.DecodeString(args.ResourceID)
+	if err != nil {
+		return err
+	}
+	ledgerState, err := t.ledger.GetStateSnapshot()
+	if err != nil {
+		return err
+	}
+	result.SplitContract = ledgerState.GetSplitContract(resourceID)
 	return nil
 }
