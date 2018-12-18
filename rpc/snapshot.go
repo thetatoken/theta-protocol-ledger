e7bf7beae6d256224eb792a59794580abc3cbffb
Qinwei Gong <qinwei@sliver.tv>
2018-12-18 11:41:12 -0800

    include metadata in snapshot

--- rpc/snapshot.go
+++ rpc/snapshot.go
@@ -13,6 +13,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"github.com/thetatoken/ukulele/common"
+	"github.com/thetatoken/ukulele/ledger/state"
 	"github.com/thetatoken/ukulele/ledger/types"
 	"github.com/thetatoken/ukulele/rlp"
 	"github.com/thetatoken/ukulele/store/treestore"
@@ -28,6 +29,10 @@ type snapshotRecord struct {
 	R common.Bytes // account root, if any
 }
 
+type snapshotMetadata struct {
+	Height uint64
+}
+
 type GenSnapshotArgs struct {
 }
 
@@ -39,14 +44,16 @@ func (t *ThetaRPCServer) GenSnapshot(r *http.Request, args *GenSnapshotArgs, res
 	if err != nil {
 		return err
 	}
-	s := t.consensus.GetSummary()
 	currentTime := time.Now().UTC()
-	file, err := os.Create("theta_snapshot-" + s.Root.String() + "-" + strconv.Itoa(int(s.LastVoteHeight)) + "-" + currentTime.Format("2006-01-02"))
+	file, err := os.Create("theta_snapshot-" + sv.Hash().String() + "-" + strconv.Itoa(int(sv.Height())) + "-" + currentTime.Format("2006-01-02"))
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 	writer := bufio.NewWriter(file)
+
+	metadata := &snapshotMetadata{Height: sv.Height()}
+	writeMetadata(writer, metadata)
 
 	db := t.ledger.State().DB()
 	sv.GetStore().Traverse(nil, func(k, v common.Bytes) bool {
@@ -80,20 +87,22 @@ func (t *ThetaRPCServer) LoadSnapshot(r *http.Request, args *GenSnapshotArgs, re
 	}
 	defer file.Close()
 	reader := bufio.NewReader(file)
+
+	metadata, err := readMetadata(reader)
+	if err != nil {
+		log.Errorf("Failed to load snapshot metadata")
+		return err
+	}
+
 	db := t.ledger.State().DB()
-	store := treestore.NewTreeStore(common.Hash{}, db)
-	sizeBytes := make([]byte, 4)
+	store := state.NewStoreView(metadata.Height, common.Hash{}, db)
 	var account *types.Account
 	accountStorage := treestore.NewTreeStore(common.Hash{}, db)
 	for {
-		record, err := readRecord(reader, sizeBytes)
+		record, err := readRecord(reader)
 		if err != nil {
 			if err == io.EOF {
-				_, err := store.Commit()
-				if err != nil {
-					log.Errorf("Failed to commit store")
-					return err
-				}
+				store.Save()
 				break
 			}
 			log.Errorf("Failed to read snapshot record")
@@ -125,13 +134,47 @@ func (t *ThetaRPCServer) LoadSnapshot(r *http.Request, args *GenSnapshotArgs, re
 			accountStorage.Set(record.K, record.V)
 		}
 	}
+	t.ledger.ResetState(metadata.Height, store.Hash())
 	return
 }
 
-func readRecord(reader *bufio.Reader, sizeBytes []byte) (*snapshotRecord, error) {
+func writeMetadata(writer *bufio.Writer, metadata *snapshotMetadata) {
+	raw, err := rlp.EncodeToBytes(metadata)
+	if err != nil {
+		panic("Failed to encode snapshot metadata")
+	}
+	// write length first
+	_, err = writer.Write(itobs(uint64(len(raw))))
+	if err != nil {
+		panic("Failed to write snapshot metadata length")
+	}
+	// write metadata itself
+	_, err = writer.Write(raw)
+	if err != nil {
+		panic("Failed to write snapshot metadata")
+	}
+}
+
+func readMetadata(reader *bufio.Reader) (*snapshotMetadata, error) {
+	metadata := &snapshotMetadata{}
+	sizeBytes := make([]byte, 8)
+	_, err := reader.Read(sizeBytes)
+	if err != nil {
+		return metadata, err
+	}
+	size := bstoi(sizeBytes)
+	metadataBytes := make([]byte, size)
+	_, err = reader.Read(metadataBytes)
+	if err != nil {
+		return metadata, err
+	}
+	err = rlp.DecodeBytes(metadataBytes, metadata)
+	return metadata, err
+}
+
+func readRecord(reader *bufio.Reader) (*snapshotRecord, error) {
 	record := &snapshotRecord{}
-	// sizeBytes = sizeBytes[:0]
-	sizeBytes = make([]byte, 4)
+	sizeBytes := make([]byte, 8)
 	_, err := reader.Read(sizeBytes)
 	if err != nil {
 		return record, err
@@ -152,7 +195,7 @@ func writeRecord(writer *bufio.Writer, k, v, r common.Bytes) {
 		panic("Failed to encode storage record")
 	}
 	// write length first
-	_, err = writer.Write(itobs(len(raw)))
+	_, err = writer.Write(itobs(uint64(len(raw))))
 	if err != nil {
 		panic("Failed to write storage record length")
 	}
@@ -163,18 +206,18 @@ func writeRecord(writer *bufio.Writer, k, v, r common.Bytes) {
 	}
 }
 
-func itobs(val int) []byte {
-	arr := make([]byte, 4) // assume record length won't exceed 9999
-	for i := 0; i < 4; i++ {
+func itobs(val uint64) []byte {
+	arr := make([]byte, 8)
+	for i := 0; i < 8; i++ {
 		arr[i] = byte(val % 10)
 		val /= 10
 	}
 	return arr
 }
 
-func bstoi(arr []byte) (val int) {
-	for i := 0; i < 4; i++ { // same assumption
-		val = val + int(arr[i])*int(math.Pow10(i))
+func bstoi(arr []byte) (val uint64) {
+	for i := 0; i < 8; i++ {
+		val = val + uint64(arr[i])*uint64(math.Pow10(i))
 	}
 	return
 }
