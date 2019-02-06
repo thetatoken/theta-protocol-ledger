3e127990a85db2adcb8e8d746bb5eb92558009b8
Jieyi Long <jieyi@sliver.tv>
2019-02-06 15:17:24 -0800

    Default config changes

--- core/block.go
+++ core/block.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	// MaxNumRegularTxsPerBlock represents the max number of regular transaction can be inclulded in one block
-	MaxNumRegularTxsPerBlock int = 1024
+	MaxNumRegularTxsPerBlock int = 16384
 )
 
 var (
