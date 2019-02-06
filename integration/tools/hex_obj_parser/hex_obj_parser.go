c7f7f8ce6d8084bf7210be820f1838251d55b005
Jieyi Long <jieyi@sliver.tv>
2019-02-05 16:58:12 -0800

    Extend tx parser to general hex object parser

---
+++ integration/tools/hex_obj_parser/hex_obj_parser.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	"github.com/thetatoken/theta/core"
+	"github.com/thetatoken/theta/ledger/types"
+	"github.com/thetatoken/theta/rlp"
+)
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+func printUsage() {
+	fmt.Println("Usage: hex_obj_parser <obj_hex>")
+}
+
+func txFromBytes(raw []byte) error {
+	tx, err := types.TxFromBytes(raw)
+	if err == nil {
+		fmt.Printf("\nTx: %v\n\n", tx)
+		return nil
+	}
+	return fmt.Errorf("Not a transaction")
+}
+
+func voteFromBytes(raw []byte) error {
+	vote := core.Vote{}
+	err := rlp.DecodeBytes(raw, &vote)
+	if err == nil {
+		fmt.Printf("\nVote: %v\n\n", vote)
+		return nil
+	}
+	return fmt.Errorf("Not a vote object")
+}
+
+func blockFromBytes(raw []byte) error {
+	block := core.ExtendedBlock{}
+	err := rlp.DecodeBytes(raw, &block)
+	if err == nil {
+		fmt.Printf("\nBlock: %v\n\n", block)
+		return nil
+	}
+	return fmt.Errorf("Not an extended block object")
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		printUsage()
+		return
+	}
+
+	raw, err := hex.DecodeString(args[0])
+	handleError(err)
+
+	handlers := []func(raw []byte) error{
+		blockFromBytes,
+		voteFromBytes,
+		txFromBytes,
+	}
+	for _, handler := range handlers {
+		err := handler(raw)
+		if err == nil {
+			return
+		}
+	}
+	err = fmt.Errorf("Unable to identity the type of the object")
+	handleError(err)
+}
