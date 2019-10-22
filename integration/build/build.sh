0320fc28d3da587127e67e9d0cb4e38ccd7bf489
Ribao Wei <ribao@sliver.tv>
2019-10-22 13:13:24 -0700

    Update docker build script.

--- integration/build/build.sh
+++ integration/build/build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Usage: 
+#    integration/build/build.sh
+#    integration/build/build.sh force # Always recreate docker image and container.
 set -e
 
 SCRIPTPATH=$(dirname "$0")
 
 echo $SCRIPTPATH
 
-if [[ "$(docker images -q theta_builder 2> /dev/null)" == "" ]]; then
-    docker build -t theta_builder $SCRIPTPATH
+if [ "$1" =  "force" ] || [[ "$(docker images -q theta_builder_image 2> /dev/null)" == "" ]]; then
+    docker build -t theta_builder_image $SCRIPTPATH
 fi
 
-docker run -it -v "$GOPATH:/go" theta_builder
 
+docker stop theta_builder
+docker rm theta_builder
+
+docker run --name theta_builder -it -v "$GOPATH:/go" theta_builder_image
