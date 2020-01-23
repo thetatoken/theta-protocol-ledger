117c8b697c4355db5a05aca43a177aa82fc1c44e
Ribao Wei <ribao@sliver.tv>
2020-01-23 15:23:29 -0800

    Update example

--- integration/docker/node/build.sh
+++ integration/docker/node/build.sh
@@ -7,7 +7,7 @@
 # After the image is built, you can create a container by:
 #    docker stop theta_node
 #    docker rm theta_node
-#    docker run --name theta_node -it theta
+#    docker run -e THETA_CONFIG_PATH=/theta/integration/privatenet/node --name theta_node -it theta
 set -e
 
 SCRIPTPATH=$(dirname "$0")
