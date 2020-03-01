a0df93d82a5189dc804da8aa264b1065de8088be
Jieyi Long <jieyi@sliver.tv>
2020-03-01 11:47:07 -0800

    Rename portL to libp2pPort

--- common/config.go
+++ common/config.go
@@ -51,7 +51,7 @@ const (
 	// CfgP2PPort sets the port used by P2P network.
 	CfgP2PPort = "p2p.port"
 	// CfgP2PLPort sets the port used by P2P network.
-	CfgP2PLPort = "p2p.portL"
+	CfgP2PLPort = "p2p.libp2pPort"
 	// CfgP2PSeeds sets the boostrap peers.
 	CfgP2PSeeds = "p2p.seeds"
 	// CfgLibP2PSeeds sets the boostrap peers in libp2p format.
