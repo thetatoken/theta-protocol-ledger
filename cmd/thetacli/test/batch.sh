f931680d89743b7a4a890cece604b4529a18633f
Andrew Lunde <andrew.lunde@sap.com>
2021-06-11 18:51:37 -0400

    added next sequence

--- cmd/thetacli/test/batch.sh
+++ cmd/thetacli/test/batch.sh
@@ -4,118 +4,32 @@ do_run=1 # Execute(evaluate) the commands
 #do_run=0 # Don't evaluate any commands
 #do_echo=1 # Echo the commands
 do_echo=0 # Don't echo any commands
-do_echo_on_chain=1 # Echo the on-chain commands
-#do_echo_on_chain=0 # Don't echo on-chain commands
+#do_echo_on_chain=1 # Echo the on-chain commands
+do_echo_on_chain=0 # Don't echo, actually execute on-chain commands
+#calc_costs_only=1 # Only calculate the costs
+calc_costs_only=0 # Calculate the costs then run tx
 
 Alice=2E833968E5bB786Ae419c4d13189fB081Cc43bab
 Bob=70f587259738cB626A1720Af7038B8DcDb6a42a0
 Carol=cd56123D0c5D6C1Ba4D39367b88cba61D93F5405
 rid=rid1000001
 
-rpduration=101
+#rfdurationblocks=101       # Reserve Fund Duration in blocktimes : usualy (101 x 6secs) = 606 / 60 = 10.1 minutes
+rfdurationblocks=30        # Reserve Fund Duration in blocktimes : Minimum for testing = 30 = 3 minues
+#rfdurationsecs=0          # Reserve Fund Duration in seconds : 0 = Use rfdurationblocks instead
+#let rfdurationsecs=120     # Reserve Fund Duration in seconds
+let rfdurationsecs=10*60    # 10 minutes
+#let rfdurationsecs=3*60*60 # 3 hours
 
 rfund=100
 rcoll=101
 
-# Cost tests performed at Thu Jun 10 13:17:06 EDT 2021
-#tfuel=100.0
-#Thu Jun 10 13:19:07 EDT 2021
-#TFuel(USD): 0.54810887040421
-#USD/Transaction: .16
-#100.0 TFuel in USD = 54.8109 x 10 items = 548.1090 USD
-#visacost USD: 7.57 to send 10 items (1.3800%)margin
-#mn30cost USD: 1.60 to send 10 items (.2900%)margin
-#
-#tfuel=10.0
-#Thu Jun 10 13:19:40 EDT 2021
-#TFuel(USD): 0.54810887040421
-#USD/Transaction: .16
-#10.0 TFuel in USD = 5.4811 x 10 items = 54.8110 USD
-#visacost USD: 1.21 to send 10 items (2.2000%)margin
-#mn30cost USD: 1.60 to send 10 items (2.9100%)margin
-#
-#tfuel=1.0
-#Thu Jun 10 13:20:36 EDT 2021
-#TFuel(USD): 0.54810887040421
-#USD/Transaction: .16
-#1.0 TFuel in USD = .5481 x 10 items = 5.4810 USD
-#visacost USD: .57 to send 10 items (10.3900%)margin
-#mn30cost USD: 1.60 to send 10 items (29.1900%)margin
-#
 #tfuel=0.1
-#Thu Jun 10 13:21:20 EDT 2021
-#TFuel(USD): 0.54810887040421
-#USD/Transaction: .16
-#0.1 TFuel in USD = .0548 x 10 items = .5480 USD
-#visacost USD: .51 to send 10 items (93.0600%)margin
-#mn30cost USD: 1.60 to send 10 items (291.9700%)margin
-#
-#tfuel=0.01
-#Thu Jun 10 13:22:17 EDT 2021
-#TFuel(USD): 0.55011933917353
-#USD/Transaction: .17
-#0.01 TFuel in USD = .0055 x 10 items = .0550 USD
-#visacost USD: .50 to send 10 items (909.0900%)margin
-#mn30cost USD: 1.70 to send 10 items (3090.9000%)margin
-#
-# Now these are off-chain micropayment scenarios
-# performed Fri Jun 11 11:56:06 EDT 2021
-#
-#Fri Jun 11 11:56:06 EDT 2021
-#TFuel(USD): 0.48622932399761
-#USD/Transaction: .15
-#0.01 TFuel in USD = .0049 x 1000 items = 4.9000 USD
-#visacost USD: 50.06 to send 1000 items (1021.6300%)margin
-#mn30cost USD: 150.00 to send 1000 items (3061.2200%)margin
-#mn30cost USD: .15 to send 1000 items (3.061200%)margin(1 service_payment)
-#
-#Fri Jun 11 11:58:38 EDT 2021
-#TFuel(USD): 0.48603013940903
-#USD/Transaction: .15
-#0.001 TFuel in USD = .0005 x 10000 items = 5.0000 USD
-#visacost USD: 500.06 to send 10000 items (10001.2000%)margin
-#mn30cost USD: 1500.00 to send 10000 items (30000.0000%)margin
-#mn30cost USD: .15 to send 10000 items (3.000000%)margin(1 service_payment)
-#
-# WSJ.com example : $38.99 for 4 weeks = $1.3925 per day = $4.1775 per 3 days
-# Subscriber reads 10 articles over 3 days on average.  Each article Header + blurb free
-# Once article clicked on.  First min charged, additional min/read charged as scrolled to : avg 5 mins/article
-# 10 article * 5 min/read/article = 50 transactions over 3 days.
-# If subsriber clicks on 0 articles over 3 days, no charges for that time
-# Once user clicks on next article, new reserve fund is created for next 3 days
-#
-#Fri Jun 11 12:22:15 EDT 2021
-#TFuel(USD): 0.48705293232693
-#USD/Transaction: .15
-#0.2 TFuel in USD = .0974 x 50 items = 4.8700 USD
-#visacost USD: 2.56 to send 50 items (52.5600%)margin
-#mn30cost USD: 7.50 to send 50 items (154.0000%)margin
-#mn30cost USD: .15 to send 50 items (3.080000%)margin(1 service_payment)
-#
-# Daily Reader 3-4 articles/day
-# 4.8700+0.15 x 10(3day periods/month) = $48.85/month 
-#
-# Weekend Reader
-# 4.8700+0.15 x 4(3day periods/month) = $19.54/month
-#
-# Sporatic Reader : 2 articles/day on 10 days spread evenly across the month
-# 0.963+0.15 x 10 = $9.78/month
-# 
-# Occasional Reader : 4 articles/month spread evenly across the month
-# 0.4815+0.15 x 4 = $2.53/month
-#
-# Rare Reader : 1 article/month
-# 0.4815+0.15 x 1 = $0.63/month
-#
-# Rare Reader Aborted Article : 1/5 article/month
-# 0.0963+0.15 x 1 = $0.25/month
-
-
-tfuel=0.2
+tfuel=2
 
 # total 10 items
 bobsigs=10
-carolsigs=0
+carolsigs=10
 
 tfuelfee=0.3
 tfuelperc=0.0
@@ -218,7 +132,7 @@ let moviesecs=120*60 # 90mins + 30 for pausing to pee and make popcorn
 cmd='deltablocks=$(echo "scale=0; ('$moviesecs' / '$aspb')/1" | bc -q 2>/dev/null)'
 if [ $do_echo -eq 1 ]; then echo $cmd; fi
 if [ $do_run -eq 1 ]; then eval $cmd; fi
-echo "deltablocks: "$deltablocks
+echo "deltablocks: "$deltablocks" to watch a movie."
 
 echo -n "          Current Date-Time : " ; date -r $cbt
 #dis=$(date +%s)
@@ -287,7 +201,7 @@ if [ $do_echo -eq 1 ]; then echo $cmd; fi
 if [ $do_run -eq 1 ]; then eval $cmd; fi
 echo "mn30cost USD: "$mn30cost" to send "$totalitems" items ("$mn30margin"%)margin(1 service_payment)"
 
-#exit
+if [ $calc_costs_only -eq 1 ]; then echo ""; exit; fi
 
 #aib=$(thetacli query account --address=$Alice | tail -n +2 | jq .coins.tfuelwei | tr -d '"'"'"'"'"')
 echo ""
@@ -330,7 +244,15 @@ if [ $do_run -eq 1 ]; then eval $cmd; fi
 if [ ${#arf} == 2 ]; then
     echo "No Reserve Fund"
     echo "Alice begin create reserve."
-    cmd='thetacli tx reserve --chain="privatenet" --async --from='$Alice' --fund='$rfund' --collateral='$rcoll' --duration='$rpduration' --resource_ids='$rid' --seq='$ans' --pw=qwertyuiop'
+    if [ $rfdurationsecs -eq 0 ]; then
+        echo "Using "$rfdurationblocks" blocks"
+    else
+        cmd='rfdurationblocks=$(echo "scale=0; ('$rfdurationsecs' / '$aspb')/1" | bc -q 2>/dev/null)'
+        if [ $do_echo -eq 1 ]; then echo $cmd; fi
+        if [ $do_run -eq 1 ]; then eval $cmd; fi
+        echo "Using: "$rfdurationblocks" blocks = "$rfdurationsecs"secs"
+    fi
+    cmd='thetacli tx reserve --chain="privatenet" --async --from='$Alice' --fund='$rfund' --collateral='$rcoll' --duration='$rfdurationblocks' --resource_ids='$rid' --seq='$ans' --pw=qwertyuiop'
     if [ $do_echo -eq 1 ]; then echo $cmd; fi
     if [ $do_run -eq 1 ]; then eval $cmd; fi
     echo "Alice end create reserve.  Wait 15 seconds and rereun."
@@ -346,19 +268,31 @@ else
     echo "reserve_sequence:"$resseq
     echo "end_block_height:"$ebh
     echo "current_block_height:"$(thetacli query status | jq .current_height | tr -d '"')
+    # Find the largest existing transfer_record sequence number
+    # thetacli query account --address=2E833968E5bB786Ae419c4d13189fB081Cc43bab | jq .reserved_funds[0].transfer_records[-1].service_payment.payment_sequence | tr -d '"'
+    cmd='txlen=$(thetacli query account --address='$Alice' | jq .reserved_funds[0].transfer_records)'
+    if [ $do_echo -eq 1 ]; then echo $cmd; fi
+    if [ $do_run -eq 1 ]; then eval $cmd; fi
+    if [ ${#txlen} == 2 ]; then
+        echo "No Transfer Records exit yet."
+    else 
+        echo "Transfer Records exit."
+        cmd='letxseq=$(thetacli query account --address='$Alice' | jq .reserved_funds[0].transfer_records[-1].service_payment.payment_sequence | tr -d '"'"'"'"'"')'
+        if [ $do_echo -eq 1 ]; then echo $cmd; fi
+        if [ $do_run -eq 1 ]; then eval $cmd; fi
+        startseq=$letxseq
+        echo "Setting startseq = "$letxseq
+    fi
 fi
-
-aab=$(thetacli query account --address=$Alice | jq .coins.tfuelwei | tr -d '"')
-bab=$(thetacli query account --address=$Bob | jq .coins.tfuelwei | tr -d '"')
-
-echo ""
-echo "Alice after balance: "$(un_wei $aab)
-echo "Bob after balance: "$(un_wei $bab)
-
-
 aib=$(thetacli query account --address=$Alice | jq .coins.tfuelwei | tr -d '"')
 bib=$(thetacli query account --address=$Bob | jq .coins.tfuelwei | tr -d '"')
 cib=$(thetacli query account --address=$Carol | jq .coins.tfuelwei | tr -d '"')
+
+echo ""
+echo "Alice init balance: "$(un_wei $aib)
+echo "Bob init balance: "$(un_wei $bib)
+echo "Carol init balance: "$(un_wei $cib)
+
 
 aseq=$(thetacli query account --address=$Alice | jq .sequence | tr -d '"')
 ans=$(( $aseq + 1 ))
@@ -369,7 +303,7 @@ echo ""
 echo "Begin off-chain signature generation."
 echo ""
 
-start=`date +%s`
+offstart=`date +%s`
 
 i=0
 tfuelamt=0
@@ -406,7 +340,7 @@ while [ $i -lt $carolsigs ]; do
         tfuelamt=$tfuel 
     fi
     echo "Carol:"$payseq" = "$tfuelamt
-    #cmd='sigout=$(./sp.sh --from='$Alice' --to='$Carol' --payment_seq='$i' --reserve_seq='$resseq' --resource_id='$rid' --tfuel='$tfuel' | tail -n +6 | jq .source.signature | tr -d '"'"'"'"'"')'
+    #cmd='sigout=$(./sp.sh --from='$Alice' --to='$Bob' --payment_seq='$i' --reserve_seq='$resseq' --resource_id='$rid' --tfuel='$tfuel' | tail -n +6 | jq .source.signature | tr -d '"'"'"'"'"')'
     cmd='sigout=$(thetacli tx service_payment --chain="privatenet" --from='$Alice' --to='$Carol' --payment_seq='$payseq' --reserve_seq='$resseq' --resource_id='$rid' --tfuel='$tfuelamt' --pw=qwertyuiop | jq .source.signature | tr -d '"'"'"'"'"')'
     if [ $do_echo -eq 1 ]; then echo $cmd; fi
     if [ $do_run -eq 1 ]; then eval $cmd; fi
@@ -424,14 +358,17 @@ echo ""
 echo "End off-chain signature generation."
 echo ""
 
-end=`date +%s`
-let deltatime=end-start
+offend=`date +%s`
+let deltatime=offend-offstart
 let hours=deltatime/3600
 let minutes=(deltatime/60)%60
 let seconds=deltatime%60
-printf "Off-Chain Time spent: %d:%02d:%02d\n" $hours $minutes $seconds
+cmd='secspertx=$(echo "scale=3; ('$deltatime' / ('$bobsigs' + '$carolsigs'))/1" | bc -q 2>/dev/null)'
+if [ $do_echo -eq 1 ]; then echo $cmd; fi
+if [ $do_run -eq 1 ]; then eval $cmd; fi
+printf "Off-Chain Time spent: %d:%02d:%02d (%2.5f sec/tx)\n" $hours $minutes $seconds $secspertx
 
-start=`date +%s`
+onstart=`date +%s`
 
 echo ""
 echo "Begin on-chain service-payment transactions."
@@ -439,6 +376,7 @@ echo ""
 
 i=0
 tfuelamt=0
+baccum=0
 let holdcnt=bobsigs-1
 while [ $i -lt $bobsigs ]; do
     #echo "i:"$i
@@ -451,30 +389,33 @@ while [ $i -lt $bobsigs ]; do
     else 
         tfuelamt=$tfuel 
     fi
-    if [ $accumulate -eq 1 ]; then
-        if [ $i -lt $holdcnt ]; then
-            echo "Hold::: Bob["$payseq"]"
-        else
-            echo "Submit: Bob["$payseq"]"
-        fi
-    else
-        echo "Bob["$payseq"]"
-    fi
 
     #cmd='./sp.sh --from='$Alice' --to='$Bob' --payment_seq='$payseq' --reserve_seq='$ans' --resource_id='$rid' --tfuel='$tfuel' --on_chain --src_sig='$sig
     cmd='thetacli tx service_payment --chain="privatenet" --from='$Alice' --to='$Bob' --payment_seq='$payseq' --reserve_seq='$resseq' --resource_id='$rid' --tfuel='$tfuelamt' --pw=qwertyuiop --on_chain --src_sig='$sig
-    if [ $do_echo -eq 1 ]; then echo $cmd; fi
-    if [ $do_echo_on_chain -eq 1 ]; then
-        echo $cmd
-        echo ""
+
+    if [ $accumulate -eq 1 ]; then
+        if [ $i -lt $holdcnt ]; then
+            echo "Hold::: Bob["$payseq"]"
+            if [ $do_echo -eq 1 ]; then echo $cmd; fi
+            if [ $do_echo_on_chain -eq 1 ]; then echo $cmd; echo ""; fi
+        else
+            echo "Submit: Bob["$payseq"]"
+            let baccum++
+            if [ $do_echo -eq 1 ]; then echo $cmd; fi
+            if [ $do_echo_on_chain -eq 1 ]; then echo $cmd; echo ""; else if [ $do_run -eq 1 ]; then eval $cmd; fi fi
+       fi
     else
-        if [ $do_run -eq 1 ]; then eval $cmd; fi
+        echo "Bob["$payseq"]"
+        if [ $do_echo -eq 1 ]; then echo $cmd; fi
+        if [ $do_echo_on_chain -eq 1 ]; then echo $cmd; echo ""; else if [ $do_run -eq 1 ]; then eval $cmd; fi fi
     fi
+
     let i++
 done
 
 i=0
 tfuelamt=0
+caccum=0
 let holdcnt=carolsigs-1
 while [ $i -lt $carolsigs ]; do
     #echo "i:"$i
@@ -487,25 +428,27 @@ while [ $i -lt $carolsigs ]; do
     else 
         tfuelamt=$tfuel 
     fi
-    if [ $accumulate -eq 1 ]; then
-        if [ $i -lt $holdcnt ]; then
-            echo "Hold::: Carol["$payseq"]"
-        else
-            echo "Submit: Carol["$payseq"]"
-        fi
-    else
-        echo "Carol["$payseq"]"
-    fi
 
     #cmd='./sp.sh --from='$Alice' --to='$Bob' --payment_seq='$payseq' --reserve_seq='$ans' --resource_id='$rid' --tfuel='$tfuel' --on_chain --src_sig='$sig
     cmd='thetacli tx service_payment --chain="privatenet" --from='$Alice' --to='$Carol' --payment_seq='$payseq' --reserve_seq='$resseq' --resource_id='$rid' --tfuel='$tfuelamt' --pw=qwertyuiop --on_chain --src_sig='$sig
-    if [ $do_echo -eq 1 ]; then echo $cmd; fi
-    if [ $do_echo_on_chain -eq 1 ]; then
-        echo $cmd
-        echo ""
+
+    if [ $accumulate -eq 1 ]; then
+        if [ $i -lt $holdcnt ]; then
+            echo "Hold::: Carol["$payseq"]"
+            if [ $do_echo -eq 1 ]; then echo $cmd; fi
+            if [ $do_echo_on_chain -eq 1 ]; then echo $cmd; echo ""; fi
+        else
+            echo "Submit: Carol["$payseq"]"
+            let caccum++
+            if [ $do_echo -eq 1 ]; then echo $cmd; fi
+            if [ $do_echo_on_chain -eq 1 ]; then echo $cmd; echo ""; else if [ $do_run -eq 1 ]; then eval $cmd; fi fi
+       fi
     else
-        if [ $do_run -eq 1 ]; then eval $cmd; fi
+        echo "Carol["$payseq"]"
+        if [ $do_echo -eq 1 ]; then echo $cmd; fi
+        if [ $do_echo_on_chain -eq 1 ]; then echo $cmd; echo ""; else if [ $do_run -eq 1 ]; then eval $cmd; fi fi
     fi
+
     let i++
 done
 
@@ -513,18 +456,29 @@ echo ""
 echo "End on-chain service-payment transactions."
 echo ""
 
-end=`date +%s`
-let deltatime=end-start
+onend=`date +%s`
+let deltatime=onend-onstart
 let hours=deltatime/3600
 let minutes=(deltatime/60)%60
 let seconds=deltatime%60
-printf "On-Chain Time spent: %d:%02d:%02d\n" $hours $minutes $seconds
+cmd='secspertx=$(echo "scale=3; ('$deltatime' / ('$baccum' + '$caccum'))/1" | bc -q 2>/dev/null)'
+if [ $do_echo -eq 1 ]; then echo $cmd; fi
+if [ $do_run -eq 1 ]; then eval $cmd; fi
+printf "On-Chain Time spent: %d:%02d:%02d (%2.5f sec/tx)\n" $hours $minutes $seconds $secspertx
 
+let deltatime=onend-offstart
+let hours=deltatime/3600
+let minutes=(deltatime/60)%60
+let seconds=deltatime%60
+cmd='secspertx=$(echo "scale=3; ('$deltatime' / ('$baccum' + '$caccum' + '$bobsigs' + '$carolsigs'))/1" | bc -q 2>/dev/null)'
+if [ $do_echo -eq 1 ]; then echo $cmd; fi
+if [ $do_run -eq 1 ]; then eval $cmd; fi
+printf "Total TX Time spent: %d:%02d:%02d (%2.5f sec/tx)\n" $hours $minutes $seconds $secspertx
 
 
 aab=$(thetacli query account --address=$Alice | jq .coins.tfuelwei | tr -d '"')
 bab=$(thetacli query account --address=$Bob | jq .coins.tfuelwei | tr -d '"')
-cab=$(thetacli query account --address=$Carol | jq .coins.tfuelwei | tr -d '"')
+cax=$(thetacli query account --address=$Carol | jq .coins.tfuelwei | tr -d '"')
 
 echo ""
 echo "Alice initial balance: "$(un_wei $aib)
@@ -534,6 +488,6 @@ echo "Bob initial balance  : "$(un_wei $bib)
 echo "Bob final balance    : "$(un_wei $bab)
 echo ""
 echo "Carol initial balance: "$(un_wei $cib)
-echo "Carol final balance  : "$(un_wei $cab)
+echo "Carol final balance  : "$(un_wei $cax)
 echo ""
 echo "Finished Batch Test."
